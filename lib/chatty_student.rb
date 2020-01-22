f19b97aa353d989c6bb0cba060265d558ade7b8e
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-22 16:23:20 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,8 +2,11 @@
   def hello
     super
     @hello = hello
-    puts 
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
+  
+  def raise_hand
+    super
 end
   
 end
